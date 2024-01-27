c96233dcce99250666cbc672279c52ac28ef7ec1
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2024-01-27 12:05:10 -0800

    3 languages

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,16 @@
+/**
+ * Write a script that prints 3 lines:
+   The first line: “C is fun”
+   The second line: “Python is cool”
+   The third line: “JavaScript is amazing”
+   You must use console.log(...) to print all output
+   You are not allowed to use var
+ */
+
+   let firstLine = 'C is fun'
+   let secondLine = 'Python is cool'
+   const thirdLine = 'JavaScript is amazing'
+
+   console.log(firstLine)
+   console.log(secondLine)
+   console.log(thirdLine)
