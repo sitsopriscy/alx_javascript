5703a2d009461cb117bd82b5263ab39bc5139ed4
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2024-01-27 13:08:06 -0800

    Object

---
+++ javascript-warm_up/12-object.js
@@ -0,0 +1,27 @@
+/**
+ * Update this script to replace the value 12 with 89:
+ * You are not allowed to use var
+ */
+/**
+ * const myObject = {
+    type: 'object',
+    value: 12
+  };
+  console.log(myObject);
+
+  YOUR CODE HERE
+
+  console.log(myObject);
+ */
+
+
+  const myObject = {
+    type: 'object',
+    value: 12
+  };
+  console.log(myObject);
+
+  myObject.value = 89
+
+  console.log(myObject);
+  
