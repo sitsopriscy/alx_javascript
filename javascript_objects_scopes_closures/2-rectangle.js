722ef5b4a038b202a5b9a97512b8b50233125f47
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2024-01-30 15:03:14 -0800

    Rectangle2.1

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -6,6 +6,8 @@
 // Initialize the instance attribute height with the value of h
 // If w or h is equal to 0 or not a positive integer, create an empty object
 
+
+// #!/usr/bin/node
 class Rectangle {
     constructor(w, h) {
       if (w > 0 && h > 0) {
