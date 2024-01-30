b8c831653027bace7a3bf7f0f23a27914d22be24
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2024-01-30 12:44:50 -0800

    callMeMoby

--- javascript-warm_up/101-call_me_moby.js
+++ javascript-warm_up/101-call_me_moby.js
@@ -7,32 +7,11 @@
 
 
 
-// const objectXtimes = {
-//     Xtimes: function (x, theFunction) {
-//         for (let i = 0; i < x; i++) {
-//             theFunction()
-//         }
-//     }
-
-// }
-
-// Example:
-
-// objectXtimes.Xtimes(3, function() {
-//     console.log("C is fun")
-// })
-
-const objectXtimes = {
-    Xtimes: function(x, theFunction) {
-      for (let i = 0; i < x; i++) {
-        theFunction()
-      }
+exports.callMeMoby = {
+    Xtimes: function (x, theFunction) {
+        for (let i = 0; i < x; i++) {
+            theFunction()
+        }
     }
-  }
-  
-  // Example usage
-  const callMeMoby = () => console.log("C is fun")
-  
-  objectXtimes.Xtimes(5, callMeMoby)
-  
 
+}
