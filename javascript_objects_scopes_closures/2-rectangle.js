348bd89393aa95c03ef459c739387bf2d1399f07
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2024-01-30 15:05:11 -0800

    Rectangle202

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -7,7 +7,8 @@
 // If w or h is equal to 0 or not a positive integer, create an empty object
 
 
-// #!/usr/bin/node
+#!/usr/bin/node
+
 class Rectangle {
     constructor(w, h) {
       if (w > 0 && h > 0) {
