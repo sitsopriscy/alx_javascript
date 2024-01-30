5fcab93a3461b45f41fb77ac96886ea2b221e01c
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2024-01-30 14:58:35 -0800

    Rectangle 1

---
+++ javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,5 @@
+// Write a class Square that defines a square and inherits from Rectangle of 4-rectangle.js:
+
+// You must use the class notation for defining your class and extends
+// The constructor must take 1 argument: size
+// The constructor of Rectangle must be called (by using super())
