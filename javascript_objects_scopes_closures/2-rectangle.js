bb95e8303a9fec97d209354d4b0c0b5ce489e3e0
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2024-01-30 15:08:28 -0800

    rectangle203

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -11,7 +11,7 @@
 
 class Rectangle {
     constructor(w, h) {
-      if (w > 0 && h > 0) {
+        if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
         this.width = w;
         this.height = h;
       }
