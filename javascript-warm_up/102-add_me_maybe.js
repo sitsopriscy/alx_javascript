3eb5ba920164074573aaa6395e8a10347ad46f81
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2024-01-30 12:58:38 -0800

    Add me maybe

---
+++ javascript-warm_up/102-add_me_maybe.js
@@ -0,0 +1,11 @@
+// Write a function that increments and calls a function.
+
+// The function must be visible from outside
+// Prototype: function (number, theFunction)
+// You are not allowed to use var
+
+exports.addMeMaybe = function (x, theFunction) {
+    for (let i=0; i<'New value'; i++) {
+        theFunction('New value: ' + nb)
+    }
+}
