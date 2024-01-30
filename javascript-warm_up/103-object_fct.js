1927db6696856ee327683af0ce96a946a80d9231
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2024-01-30 14:39:50 -0800

    can' t get the desired result

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -1,16 +1,17 @@
 // Update this script by adding a new function incr that increments the integer value.
 // You are not allowed to use var
 
-
 const myObject = {
     type: 'object',
     value: 12
 };
 console.log(myObject);
-myObject.incr = function incr() {
-    this.function
 
-}
+myObject.incr = function () {
+    this.value++;
+
+};
+
 
 myObject.incr();
 console.log(myObject);
