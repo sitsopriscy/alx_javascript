90e08d3e52c31a89d3cb095379876ec82e8f5f44
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2024-01-31 14:07:21 -0800

    Rectangle865

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -6,3 +6,33 @@
 // Initialize the instance attribute height with the value of h
 // If w or h is equal to 0 or not a positive integer, create an empty object
 // Create an instance method called print() that prints the rectangle using the character X
+
+
+class Rectangle {
+    constructor(w, h) {
+        if (w > 0 && h > 0) {
+            this.width = w
+            this.height = h
+        }
+    }
+
+    print() {
+        if (this.width && this.height) {
+            for (let i = 0; i < this.height; i++) {
+                console.log('X'.repeat(this.width))
+            }
+        }
+    }
+}
+
+module.exports = Rectangle
+
+
+
+
+// const r1 = new Rectangle(2, 3)
+// r1.print()
+
+// const r2 = new Rectangle(10, 5)
+// r2.print()
+
