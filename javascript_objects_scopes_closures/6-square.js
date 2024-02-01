b2a858ad895cecb623a5b44dedc92cc0bafc65cf
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2024-01-31 21:12:32 -0800

    Square 077

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -3,3 +3,27 @@
 // You must use the class notation for defining your class and extends
 // Create an instance method called charPrint(c) that prints the rectangle using the character c
 // If c is undefined, use the character X
+
+
+const SquareBase = require('./5-square')
+
+class Square extends SquareBase {
+  charPrint(c) {
+    if (!c) {
+      c = 'X'
+    }
+
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width))
+    }
+  }
+}
+
+module.exports = Square
+
+
+
+// const s1 = new Square(4)
+// s1.charPrint()
+
+// s1.charPrint('C')
