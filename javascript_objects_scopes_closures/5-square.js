ca00fe523075632cfd1b24e26b5343e257ac377b
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2024-01-31 20:45:22 -0800

    Square

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -3,3 +3,23 @@
 // You must use the class notation for defining your class and extends
 // The constructor must take 1 argument: size
 // The constructor of Rectangle must be called (by using super())
+
+
+const Rectangle = require('./4-rectangle')
+
+class Square extends Rectangle {
+  constructor(size) {
+    super(size, size)
+  }
+}
+
+module.exports = Square
+
+
+
+// const s1 = new Square(4)
+// s1.print()
+
+// console.log('Double:')
+// s1.double()
+// s1.print()
