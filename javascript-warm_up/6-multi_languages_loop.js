77a925302d654a76bd42a053d248be2ac0cb055a
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2024-02-02 11:49:29 -0800

    multi-lang

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -10,11 +10,9 @@
    You must use a loop (while, for, etc.)
  */
 
-   const lines = [
-    "C is fun",
-    "Python is cool",
-    "JavaScript is amazing"
-  ];
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing']
 
-  for (let i = 0; i < lines.length; i++) {
-    console.log(lines[i])}
+for (const line of lines) {
+  console.log(line)
+}
+
