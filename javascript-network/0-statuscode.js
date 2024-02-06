6ef852899d953e6f290f3b763527c22171b64070
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2024-02-06 10:13:06 -0800

    Status code

--- javascript-network/0-statuscode.js
+++ javascript-network/0-statuscode.js
@@ -3,4 +3,11 @@
 // The status code must be printed like this: code: <status code>
 // You must use the module request (NB: It must be installed if you haven’t already installed it)
 
+// const request = require('request')
 
+const request = require('request')
+request
+  .get('http://google.com/img.png')
+  .on('response', function(response) {
+    console.log(response.statusCode)
+  })
