4d232face16018fc256482dfffe0e7cbe4ee83d6
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2024-02-08 02:38:33 -0800

    Star wars movie title1

--- javascript-network/1-starwars_title.js
+++ javascript-network/1-starwars_title.js
@@ -3,11 +3,10 @@
 // You must use the Star wars API with the endpoint https://swapi-api.alx-tools.com/api/films/:id
 // You must use the module request
 
-const  request  = require("request")
+const request = require("request")
 const endpoint = 'https://swapi-api.alx-tools.com/api/films/${id}'
 
-
-request.get(endpoint, function(error, response, body) {
+request.get(endpoint, function (error, response, body) {
     const bodyMovie = JSON.parse(body)
     console.log(bodyMovie.title)
 })
