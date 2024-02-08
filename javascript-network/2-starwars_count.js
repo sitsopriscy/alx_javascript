0ea16cc3b29887a54f67cb1fe266b3b65c9a4edd
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2024-02-08 03:40:25 -0800

    Star wars Wedge Antilles

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -3,4 +3,17 @@
 // Wedge Antilles is character ID 18 - your script must use this ID for filtering the result of the API
 // You must use the module request
 
+const request = require('request')
+const apiUrl = 'https://swapi-api.alx-tools.com/api/films/'
+
+const characterId = 18
+
+request.get(apiUrl, function (error, response, body) {
+    const filmsData = JSON.parse(body)
+    
+    const filmsWithWedge = filmsData.results.filter(function (film) {
+        return film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)
+    })
+    console.log(filmsWithWedge.length)
+})
 
