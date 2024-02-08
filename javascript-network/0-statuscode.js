04e97f9b5dde60fd0f13f4cef47f870ed6802d3b
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2024-02-08 01:55:07 -0800

    status3

--- javascript-network/0-statuscode.js
+++ javascript-network/0-statuscode.js
@@ -7,6 +7,6 @@ const request = require('request')
 
 const url = process.argv[2]
 
-request.get(url, (error, response, body) => {
-    console.log(response.statusCode)
+request.get(url, function(error, response, body){
+    console.log(`code: ${response.statusCode}`)
 });
