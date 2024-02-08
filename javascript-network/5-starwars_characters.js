d586706b5cf8c56c43f38a1d490add0edc10403e
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2024-02-08 06:05:41 -0800

    Who was playing

--- javascript-network/5-starwars_characters.js
+++ javascript-network/5-starwars_characters.js
@@ -4,4 +4,24 @@
 // You must use the Star wars API
 // You must use the module request
 
+const request = require("request")
+const movieID = process.argv[2]
+const url = `https://swapi-api.alx-tools.com/api/films/${movieID}`
 
+if (movieID > 2) {
+    request(url, function (error, response, body) {
+        if (error) {
+            return console.error(error)
+        }
+        const movies = JSON.parse(body)
+        movies.characters.forEach(function (characterURL) {
+            request(characterURL, function (error, response, body) {
+                if (error) {
+                    return console.error(error)
+                }
+                const character = JSON.parse(body)
+                console.log(character.name)
+            })
+        })
+    })
+}
