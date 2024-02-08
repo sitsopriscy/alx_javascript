824468a67197685e9dbf0a8ae0dd79edef2784c9
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2024-02-08 04:18:16 -0800

    Star wars Wedge Antilles5

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -4,16 +4,23 @@
 // You must use the module request
 
 const request = require('request')
-const apiUrl = 'https://swapi-api.alx-tools.com/api/films/'
+const apiUrl = process.argv[2]
+const endpoint = 'https://swapi-api.alx-tools.com/api/films/'
+const characterId = 18
+let count = 0
 
-
-
-request.get(apiUrl, function (error, response, body) {
-    const filmsData = JSON.parse(body)
-    const characterId = 18
-    const filmsWithWedge = filmsData.results.filter(function (film) {
-        return film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)
-    })
-    console.log(filmsWithWedge.length)
+request.get(endpoint, function (error, response, body) {
+    if (error) {
+        console.error(error.message)
+    } else {
+        const filmsData = JSON.parse(body)
+        filmsData.results.forEach(function (film) {
+            film.characters.forEach(function (character) {
+                if (character.includes(`/${characterId}/`)) {
+                    count += 1
+                }
+            })
+        })
+        console.log(count)
+    }
 })
-
