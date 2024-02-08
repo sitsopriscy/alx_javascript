59b770abe10ed0fd872b8a47844889533c899f19
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2024-02-08 05:21:17 -0800

    completed

--- javascript-network/3-request_store.js
+++ javascript-network/3-request_store.js
@@ -13,7 +13,7 @@ const file_path = process.argv[3]
 request(url, function (error, response, body) {
     if (error) {
         console.error(error)
-        return;
+        return
     }
 
     fs.writeFile(file_path, body, "utf8", function (error) {
--- javascript-network/4-completed_tasks.js
+++ javascript-network/4-completed_tasks.js
@@ -3,4 +3,29 @@
 // Only print users with completed task
 // You must use the module request
 
+const request = require('request')
+const url = 'https://jsonplaceholder.typicode.com/todos'
 
+request.get(url, function (error, response, body) {
+    if (error) {
+        console.log(error)
+        return
+    } else {
+        const listTodos = JSON.parse(body)
+        // console.log(listTodos)
+
+        const completedTasksByUser = {}
+
+        // Iterate through tasks and count completed tasks by user id
+        listTodos.forEach(function (task) {
+            if (task.completed) {
+                if (completedTasksByUser[task.userId]) {
+                    completedTasksByUser[task.userId]++
+                } else {
+                    completedTasksByUser[task.userId] = 1
+                }
+            }
+        })
+        console.log(completedTasksByUser);
+    }
+})
