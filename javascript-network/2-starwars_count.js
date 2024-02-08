d098518176bb8c366f76016cd8fbb25b6aeb5985
Priscilla Sitsofe Agboado <spagboado@gmail.com>
2024-02-08 04:23:18 -0800

    Star wars Wedge Antilles 46

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -11,8 +11,6 @@ let count = 0;
 request.get(apiUrl, (error, response, body) => {
     if (error) {
         console.error(error.message);
-    } else if (response.statusCode !== 200) {
-        console.error(response.statusCode);
     } else {
         const movieData = JSON.parse(body)
         movieData.results.forEach((film) => {
